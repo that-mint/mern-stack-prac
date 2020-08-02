1ea1389e9e7956f741156e839c7055d2bba831e5
That Mint <47940073+that-mint@users.noreply.github.com>
2020-08-02 13:10:55 +1000

    add code

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//create a .env file in the same directory of this file and put ATLAS_URI= to connect using env file
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB connected successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
+
